e03523f517595228c86e38cccfa42482abc9ff07
Mohammad Issawi <2000issawi@gmail.com>
2022-06-05 20:08:56 +0300

    Intialize glbinding and swi-prolog engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <SFML/Window.hpp>
+#include<glbinding/glbinding.h>
+#include<SWI-cpp.h>
+#include<glbinding/gl33/gl.h>
 #define WINDOW_HEIGHT 1024
 #define WINDOW_WIDTH 1024
 using namespace sf;
+using namespace gl;
 int main(int argc, char **argv)
 {
     ContextSettings ctxSettings{};
@@ -13,6 +17,8 @@ int main(int argc, char **argv)
     ctxSettings.sRgbCapable = false;
     ctxSettings.stencilBits = 0;
     Window window(VideoMode(WINDOW_HEIGHT, WINDOW_WIDTH), "Akari", 7U, ctxSettings);
+    glbinding::initialize(nullptr);
+    PlEngine plEngine(1, &argv[0]);
     while (1)
     {
         Event event;
