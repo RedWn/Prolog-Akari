71652a0e5be8a49755db35db493b01cc1e731277
RedWn <redwaniscool@gmail.com>
2022-06-05 14:00:36 +0300

    Fix cell in grid

--- project.pl
+++ project.pl
@@ -21,10 +21,9 @@ wall_num(5,8,0).
 wall_num(6,2,2).
 wall_num(7,6,1).
 
-neb_of(X,Y,[X1,Y1]):- size(H,W),(X1 is X+1,Y1 is Y,X1=<W;
-                        X1 is X,Y1 is Y+1,Y1=<H;
-                        X1 is X-1,Y1 is Y,X1>0;
-                        X1 is X,Y1 is Y-1,Y1>0).
+is_cell_valid(X,Y):- size(H,W),X > 0,Y > 0, X =< W,Y =< H.
+
+neb_of(X,Y,[X1,Y1]):- X1 is X+1,Y1 is Y,is_cell_valid(X1,Y1);X1 is X,Y1 is Y+1,is_cell_valid(X1,Y1);X1 is X-1,Y1 is Y,is_cell_valid(X1,Y1);X1 is X,Y1 is Y-1,is_cell_valid(X1,Y1).
 
 all_neb_of(X,Y,L):- findall([X1,Y1],neb_of(X,Y,[X1,Y1]), L).
 
