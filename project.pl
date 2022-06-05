4e3643aed34dfa8d4696d616e9ad06cdaea5f751
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-06-05 18:18:14 +0300

    Adjust whitespace in facts

--- project.pl
+++ project.pl
@@ -5,34 +5,34 @@ size(8, 8).
 
 % These cells are walls without numbers
 % wall(XPosition, YPosition).
-wall(3,1).
-wall(6,2).
-wall(7,2).
-wall(2,3).
-wall(4,4).
-wall(8,4).
-wall(1,5).
-wall(5,5).
-wall(7,6).
-wall(2,7).
-wall(3,7).
-wall(6,8).
+wall(3, 1).
+wall(6, 2).
+wall(7, 2).
+wall(2, 3).
+wall(4, 4).
+wall(8, 4).
+wall(1, 5).
+wall(5, 5).
+wall(7, 6).
+wall(2, 7).
+wall(3, 7).
+wall(6, 8).
 
 % These cells are walls that contain numbers.
 % wall_num(XPosition, YPosition, NumberOfAdjacentLights).
-wall_num(6,2,1).
-wall_num(2,3,2).
-wall_num(4,4,4).
-wall_num(8,4,0).
-wall_num(7,6,0).
-wall_num(2,7,3).
-wall_num(6,8,1).
+wall_num(6, 2, 1).
+wall_num(2, 3, 2).
+wall_num(4, 4, 4).
+wall_num(8, 4, 0).
+wall_num(7, 6, 0).
+wall_num(2, 7, 3).
+wall_num(6, 8, 1).
 
 % Light Cells (for testing purposes)
-light(cell(2,2)).
-light(cell(1,3)).
-light(cell(6,1)).
-light(cell(8,2)).
+light(cell(2, 2)).
+light(cell(1, 3)).
+light(cell(6, 1)).
+light(cell(8, 2)).
 
 % A cell is valid if it's positioned within the boundaries of the grid
 is_cell_valid(X, Y) :-
