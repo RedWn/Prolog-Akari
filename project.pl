7fa4d72ac37451cc54d9f281523077fd8ce3e139
Mohammad Issawi <2000issawi@gmail.com>
2022-06-07 14:09:28 +0300

    Flip upside down print functors

--- project.pl
+++ project.pl
@@ -163,13 +163,16 @@ print_grid_cell(cell(X,Y),Lit):-(wall_num(X,Y,V),write(V),write(' '),!);
 							(is_cell_valid(cell(X,Y)),Lit=1,\+ is_cell_lighted(cell(X,Y)),write(.),write(' '),!).
 							
 % print_grid helpers
-print_grid_(X,Y):-size(W,_),X>W,X0 is 1,Y0 is Y+1,nl,nl,print_grid_(X0,Y0).
-print_grid_(X,Y):-size(_,H),Y=<H,print_grid_cell(cell(X,Y),0),X0 is X+1,print_grid_(X0,Y).
+print_grid_(X,Y):-size(W,_),X>W,X0 is 1,Y0 is Y-1,nl,nl,print_grid_(X0,Y0).
+print_grid_(X,Y):-Y>0,print_grid_cell(cell(X,Y),0),X0 is X+1,print_grid_(X0,Y).
+
 % Prints the grid without lighting up normal tiles
-print_grid:-print_grid_(1,1).
+print_grid:-size(_,H), print_grid_(1,H).
+
 
 % print_grid_lit helpers
-print_grid_lit_(X,Y):-size(W,_),X>W,X0 is 1,Y0 is Y+1,nl,nl,print_grid_lit_(X0,Y0).
-print_grid_lit_(X,Y):-size(_,H),Y=<H,print_grid_cell(cell(X,Y),1),X0 is X+1,print_grid_lit_(X0,Y).
+print_grid_lit_(X,Y):-size(W,_),X>W,X0 is 1,Y0 is Y-1,nl,nl,print_grid_lit_(X0,Y0).
+print_grid_lit_(X,Y):-Y>0,print_grid_cell(cell(X,Y),1),X0 is X+1,print_grid_lit_(X0,Y).
+
 % Prints the grid with lighting up normal tiles
-print_grid_lit:-print_grid_lit_(1,1).
+print_grid_lit:-size(_,H),print_grid_lit_(1,H).
