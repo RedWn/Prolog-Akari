c5645f0663a61ee70034f28e7a2541ba13d58a81
Mohammad Issawi <2000issawi@gmail.com>
2022-06-07 17:22:33 +0300

    Get grid size

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,6 +1,14 @@
 #include<Grid.h>
+#include<SWI-cpp.h>
 Grid::Grid(){}
 Grid& Grid::instance(){
     static Grid grid;
     return grid;
+}
+void Grid::init(){
+    PlTerm sizeX, sizeY;
+    PlQuery query("size", PlTermv(sizeX, sizeY));
+    query.next_solution();
+    horizontalTilesCount = sizeX;
+    verticalTilesCount = sizeY;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include<glbinding/glbinding.h>
 #include<SWI-cpp.h>
 #include<glbinding/gl33/gl.h>
+#include<Grid.h>
 #define WINDOW_HEIGHT 1024
 #define WINDOW_WIDTH 1024
 using namespace sf;
@@ -18,7 +19,8 @@ int main(int argc, char **argv)
     ctxSettings.stencilBits = 0;
     Window window(VideoMode(WINDOW_HEIGHT, WINDOW_WIDTH), "Akari", 7U, ctxSettings);
     glbinding::initialize(nullptr);
-    PlEngine plEngine(1, &argv[0]);
+    PlEngine plEngine(2, argv);
+    GRID.init();
     while (1)
     {
         Event event;
