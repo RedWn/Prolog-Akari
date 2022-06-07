05292ac29b491c56821cf0967253413cba513cf5
Hasan Mothaffar <hasan.mozafar@gmail.com>
2022-06-07 21:08:05 +0300

    Minor formatting changes

--- project.pl
+++ project.pl
@@ -2,10 +2,6 @@
 :- use_module(ray_predicates).
 :- use_module(cell_predicates).
 :- use_module(print_utilities).
-% Size of the game's grid
-% size(NumberOfRows, NumberOfColumns).
-% Note that indexing starts from 1 for both rows and columns.
-
 
 is_cell_lighted(cell(X, Y)) :- light(cell(X, Y)), !.
 is_cell_lighted(cell(X, Y)) :-
@@ -17,16 +13,18 @@ is_cell_lighted(cell(X, Y)) :-
         LightsInXRay > 0, !;
         LightsInYRay > 0
     ).
+
 is_list_lighted([]).
 is_list_lighted([cell(X, Y) | T]) :-
     is_cell_lighted(cell(X, Y)),
     is_list_lighted(T).
- all_cells_lighted :- 
+
+all_cells_lighted :- 
     get_all_cells(List),
     is_list_lighted(List).
-% Counts the number of lights in a given list
-% exploration of kind count_light_cells(L,R) is not working
 
+% Counts the number of lights in a given list.
+% Exploration of kind count_light_cells(List, Result) is not working.
 count_light_cells__([], Accumulator, Accumulator).
 count_light_cells__([H | T], Accumulator, Result) :-
     (
@@ -37,10 +35,11 @@ count_light_cells__([H | T], Accumulator, Result) :-
     ).
 count_light_cells(List, Result) :- count_light_cells__(List, 0, Result).
 
-% Returns all the lights in List and their count in Count
+% Returns all the light cells and their count.
 get_all_light_cells(List, Count) :-
     findall(cell(X,Y), light(cell(X, Y)), List),
     length(List, Count).
+
 % Returns true if there is only one light in every single axis in the grid.
 no_double_light :- 
     \+ (
@@ -53,6 +52,7 @@ no_double_light :-
         count_light_cells(YRay, LightsCountInYRay),
         LightsCountInXRay + LightsCountInYRay >= 1
     ).
+
 get_adjacent_lights_count(cell(X, Y), Count) :-
     all_neighbors_of(cell(X, Y), AdjacentLightsList),
     count_light_cells(AdjacentLightsList, Count).
@@ -66,15 +66,18 @@ check_for_lights_of_wall_num([cell(X, Y) | T]) :-
     GoalNumberOfLights =:= ActualNumberOfLights,
     
     check_for_lights_of_wall_num(T).
+
+% check_for_lights_of_wall_num(([cell(2, 3), cell(4, 4)])). This case causes prolog to backtrack.
+% Specifically, when a cell precedes the cell (4, 4).
 light_count_correct :-
     findall(cell(X, Y), wall_num(X, Y, _), WallsWithNumbersList),
     check_for_lights_of_wall_num(WallsWithNumbersList).
-% check_for_lights_of_wall_num(([cell(2, 3), cell(4, 4)])). This case causes prolog to backtrack.
-% Specifically, when a cell precedes the cell (4, 4).
+
 solved :-
     all_cells_lighted,
     no_double_light,
     light_count_correct.
+
 % the algorithm:
 % solve:-
 %     solved,findall(lights),print(lights);(
@@ -85,20 +88,18 @@ solved :-
 %     solve;
 %     light_up_singluar_cells,
 %     solve.
- get_wall_with_equal_neighbors:-
-     wall_num(X, Y, GoalNumberOfLights),
-     all_neighbors_of(cell(X, Y), List),
-     length(List,Count),
-      GoalNumberOfLights==Count,
+
+light_up_areas_of_walls_with_equal_neighbors:-
+    wall_num(X, Y, GoalNumberOfLights),
+    all_neighbors_of(cell(X, Y), List),
+    length(List, NumberOfNeighbors),
+    GoalNumberOfLights == NumberOfNeighbors,
     light_up_list(List).
 
-    light_list([]).
-    light_list([cell(X,Y)|List]):-
-    light(cell(X,Y)),
-    light_list(List).
+light_up_list([]).
+light_up_list([cell(X, Y) | T]) :-
+    light(cell(X, Y)),
+    light_up_list(T).
 
 % mark_unavailable_cells:-
 %     should follow the rules stated in the manifest but I am still to get a decent implementaion.
-
-
-% Lit arg controls if the normal tiles that are lit should be displayed lit or not
