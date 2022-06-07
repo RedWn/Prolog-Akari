5a89aea854c7381626e36504a8ed56a46dd792db
Mohammad Issawi <2000issawi@gmail.com>
2022-06-07 14:01:34 +0300

    Implement print_grid_lit

--- project.pl
+++ project.pl
@@ -154,12 +154,22 @@ solved :-
 % light_up_singluar_cells:-
 %     find every cell that has no neighbors and have no numbered and isnt a wall and assert it as light
 
-print_grid_cell(cell(X,Y)):-(wall_num(X,Y,V),write(V),write(' '),!);
+% Lit arg controls if the normal tiles that are lit should be displayed lit or not
+print_grid_cell(cell(X,Y),Lit):-(wall_num(X,Y,V),write(V),write(' '),!);
 							(wall(X,Y),write(#),write(' '),!);
 							(light(cell(X,Y)),write(*),write(' '),!);
-							is_cell_valid(cell(X,Y)),write(.),write(' '),!.
+							(is_cell_valid(cell(X,Y)),Lit=0,write(.),write(' '),!);
+							(is_cell_valid(cell(X,Y)),Lit=1,is_cell_lighted(cell(X,Y)),write(+),write(' '),!);
+							(is_cell_valid(cell(X,Y)),Lit=1,\+ is_cell_lighted(cell(X,Y)),write(.),write(' '),!).
 							
+% print_grid helpers
 print_grid_(X,Y):-size(W,_),X>W,X0 is 1,Y0 is Y+1,nl,nl,print_grid_(X0,Y0).
-print_grid_(X,Y):-size(_,H),Y=<H,print_grid_cell(cell(X,Y)),X0 is X+1,print_grid_(X0,Y).
+print_grid_(X,Y):-size(_,H),Y=<H,print_grid_cell(cell(X,Y),0),X0 is X+1,print_grid_(X0,Y).
+% Prints the grid without lighting up normal tiles
 print_grid:-print_grid_(1,1).
 
+% print_grid_lit helpers
+print_grid_lit_(X,Y):-size(W,_),X>W,X0 is 1,Y0 is Y+1,nl,nl,print_grid_lit_(X0,Y0).
+print_grid_lit_(X,Y):-size(_,H),Y=<H,print_grid_cell(cell(X,Y),1),X0 is X+1,print_grid_lit_(X0,Y).
+% Prints the grid with lighting up normal tiles
+print_grid_lit:-print_grid_lit_(1,1).
