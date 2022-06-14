5283926cd3112fea5c9850efd171d545827a9c8d
Mohammad Issawi <2000issawi@gmail.com>
2022-06-14 16:45:57 +0300

    Add Puzzles

---
+++ kbs/12-1-13-16.pl
@@ -0,0 +1,57 @@
+% THIS FILE IS AUTO GENERATED. DO NOT EDIT DIRECTLY.
+:- module(kb, [
+    size/2,
+    wall/2,
+    wall_num/3,
+    light/1,
+    unavailable/1,
+    lit/1
+]).
+
+wall(1, 12).
+wall(12, 12).
+wall(1, 11).
+wall(4, 11).
+wall(8, 11).
+wall(9, 11).
+wall(7, 10).
+wall(5, 9).
+wall(8, 9).
+wall(9, 9).
+wall(3, 8).
+wall(12, 8).
+wall(2, 7).
+wall(9, 7).
+wall(10, 7).
+wall(3, 6).
+wall(4, 6).
+wall(11, 6).
+wall(1, 5).
+wall(10, 5).
+wall(4, 4).
+wall(5, 4).
+wall(8, 4).
+wall(6, 3).
+wall(4, 2).
+wall(5, 2).
+wall(9, 2).
+wall(12, 2).
+wall(1, 1).
+wall(12, 1).
+wall_num(4, 11, 2).
+wall_num(9, 11, 1).
+wall_num(5, 9, 2).
+wall_num(8, 9, 2).
+wall_num(2, 7, 4).
+wall_num(9, 7, 1).
+wall_num(11, 6, 4).
+wall_num(4, 4, 1).
+wall_num(8, 4, 2).
+wall_num(6, 3, 3).
+wall_num(4, 2, 1).
+wall_num(5, 2, 3).
+size(12, 12).
+
+:- dynamic unavailable/1.
+:- dynamic light/1.
+:- dynamic lit/1.
